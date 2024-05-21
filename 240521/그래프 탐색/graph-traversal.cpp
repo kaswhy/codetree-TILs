7d2cb4a4f7aabacb533dbea5b0a63a79f7109770
Soyeon Kim <112332952+kaswhy@users.noreply.github.com>
2024-05-21 17:23:03 +0900

    Title: 그래프 탐색, Time: 12ms, Memory: 0MB, Status: Passed - CodeTree

--- 240521/그래프 탐색/graph-traversal.cpp
+++ 240521/그래프 탐색/graph-traversal.cpp
@@ -21,7 +21,7 @@ int main() {
     int n, m;
     cin >> n >> m;
 
-    for(int i = 0; i<n; i++){
+    for(int i = 0; i<m; i++){
         int x, y;
         cin >> x >> y;
         graph[x].push_back(y); 
