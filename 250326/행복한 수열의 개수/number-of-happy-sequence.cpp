97d9da0ac96e58394a74180ed9b8e99fee02d346
Soyeon Kim <112332952+kaswhy@users.noreply.github.com>
2025-03-26 09:38:39 +0900

    Title: 행복한 수열의 개수, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250326/행복한 수열의 개수/number-of-happy-sequence.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+int grid[100][100];
+
+int main() {
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> grid[i][j];
+        }
+    }
+
+    int count = 0; // 행복한 수열 개수
+
+    // 행으로 이루어진 수열에서 개수 세기
+    for(int i = 0; i< n; i++) {
+        int now = 1; // 연속으로 동일한 원소가 나온 개수
+        for(int j = 1; j<n; j++) {
+            if(grid[i][j] == grid[i][j-1]) now++;
+            else now = 1;
+        }
+        if(now >= m) count++;
+    }
+
+    // 열로 이루어진 수열에서 개수 세기
+    for(int i = 0; i< n; i++) {
+        int now = 1; // 연속으로 동일한 원소가 나온 개수
+        for(int j = 1; j<n; j++) {
+            if(grid[j][i] == grid[j-1][i]) now++;
+            else now = 1;
+        }
+        if(now >= m) count++;
+    }
+
+    cout << count;
+
+    return 0;
+}
