198854e01d27456a9231f6102a74cb0466fa6330
Soyeon Kim <112332952+kaswhy@users.noreply.github.com>
2025-07-27 16:53:36 +0900

    Title: 재귀함수의 꽃, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250727/재귀함수의 꽃/the-flower-of-the-recursive-function.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+void printNumber(int n) {
+    if(n == 0) {
+        return;
+    }
+    cout << n << " ";
+    printNumber(n-1);
+    cout << n << " ";
+}
+
+int main() {
+    cin >> N;
+
+    printNumber(N);
+
+    return 0;
+}
