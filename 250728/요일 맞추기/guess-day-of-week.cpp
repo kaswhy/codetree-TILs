3ae0f0f1459929ac122ce69d712da2cb5a4f3a33
Soyeon Kim <112332952+kaswhy@users.noreply.github.com>
2025-07-28 19:51:10 +0900

    Title: 요일 맞추기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250728/요일 맞추기/guess-day-of-week.cpp
+++ 250728/요일 맞추기/guess-day-of-week.cpp
@@ -39,24 +39,25 @@ int main() {
         return 0;
     }
 
-
     // 3. m1 > m2 (ex. 5 4 3 28)
     int cnt = 0;
-    for(int i = m2; i<=m1; i++) {
+    for(int i = m2; i <= m1; i++) {
         if(i == m2) {
-            cnt -= d1;
+            cnt -= (month[m2] - d2);
             continue;
         }
         if(i == m1) {
-            cnt -= (month[m1] - d2);
+            cnt -= d1;
             break;
         }
         cnt -= month[i];
     }
-    cout << days[(cnt % 7) + 7];
-    
-
-
+    int mod = cnt % 7;
+    if(mod == 0) {
+        cout << days[mod];
+        return 0;
+    }
+    cout << days[(mod % 7) + 7];
 
     return 0;
 }
