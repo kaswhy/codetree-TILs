b2e3ea288d9514adbf7e5876704f914d3d5d7eb9
Soyeon Kim <112332952+kaswhy@users.noreply.github.com>
2025-07-28 12:14:55 +0900

    Title: Date to Date, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250728/Date to Date/date-to-date.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int m1, d1, m2, d2;
+
+int days[13] = {0, 31, 2, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+int main() {
+    cin >> m1 >> d1 >> m2 >> d2;
+
+    int cnt = 0;
+    for(int i = m1; i <= m2; i++){
+        if(i == m1) {
+            cnt += (days[i] - d1 + 1);
+            continue;
+        }
+        if(i == m2) {
+            cnt += d2;
+            break;
+        }
+        cnt += days[i];
+    }
+
+    cout << cnt;
+
+    return 0;
+}
