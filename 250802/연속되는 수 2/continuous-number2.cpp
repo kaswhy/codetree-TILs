d44e55c9744a428e813cb5ef70388f68bf29de71
Soyeon Kim <112332952+kaswhy@users.noreply.github.com>
2025-08-02 10:57:58 +0900

    Title: 연속되는 수 2, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250802/연속되는 수 2/continuous-number2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+int arr[1000];
+
+int main() {
+    cin >> N;
+
+    int tmp = 0, maxtmp = 0;
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+        if(i != 0 && arr[i] != arr[i-1]){
+            if(tmp > maxtmp) {
+                maxtmp = tmp;
+                tmp = 1;
+                continue;
+            }
+            tmp = 1;
+            continue;
+        }
+        tmp++;
+    }
+
+    if(tmp > maxtmp) {
+        maxtmp = tmp;
+    }
+
+    cout << maxtmp;
+
+    return 0;
+}
