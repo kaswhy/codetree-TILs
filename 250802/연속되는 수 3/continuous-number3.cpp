04c80c79f096c2b7a90d68b8474b0bbd64a65eaa
Soyeon Kim <112332952+kaswhy@users.noreply.github.com>
2025-08-02 11:11:51 +0900

    Title: 연속되는 수 3, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250802/연속되는 수 3/continuous-number3.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+int arr[1000];
+
+int main() {
+    cin >> N;
+
+    int tmp = 0, maxtmp = 0;
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+
+        if(i != 0 && (arr[i] * arr[i-1] < 0)) {
+            if(tmp > maxtmp) {
+                maxtmp = tmp;
+            }
+            tmp = 1;
+            continue;
+        }
+        tmp++;
+    }
+
+    if(tmp > maxtmp) {
+        maxtmp = tmp;
+    }
+
+    cout << maxtmp;
+
+    return 0;
+}
