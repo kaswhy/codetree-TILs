a779837d242512e0bc4c19d9e2d3c44cb747083f
Soyeon Kim <112332952+kaswhy@users.noreply.github.com>
2025-08-15 22:27:07 +0900

    Title: 최고의 13위치, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250815/최고의 13위치/best-place-of-13.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int N;
+int grid[20][20];
+
+int main() {
+    cin >> N;
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++) cin >> grid[i][j];
+
+    int max_coins = 0;
+    for(int i = 0; i<N; i++){
+        for(int j = 0; j<N-2; j++) {
+            max_coins = max(max_coins, grid[i][j] + grid[i][j + 1] + grid[i][j + 2]);
+        }
+    }
+
+    cout << max_coins;
+
+    return 0;
+}
