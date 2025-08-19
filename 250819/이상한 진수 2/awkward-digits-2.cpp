afe8ab7d824e359326c596ce9b7564dce23ec1ba
Soyeon Kim <112332952+kaswhy@users.noreply.github.com>
2025-08-19 22:53:22 +0900

    Title: 이상한 진수 2, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250819/이상한 진수 2/awkward-digits-2.cpp
+++ 250819/이상한 진수 2/awkward-digits-2.cpp
@@ -12,6 +12,9 @@ int main() {
     bool is_first = true;
     for(int i = 0; i<a.size(); i++) {
         if(a[i] == '1') {
+            if(is_first && i == a.size() -1) {
+                break;
+            }
             ans += pow(2, a.size() - i - 1);
             continue;
         }
