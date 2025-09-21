a7930cc62a75d1a80c272a3fbfaa0a471fa01896
Soyeon Kim <112332952+kaswhy@users.noreply.github.com>
2025-09-21 17:22:04 +0900

    Title: G or H 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250921/G or H 2/G-or-H-2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+int pos[100];
+char alpha[100];
+
+int main() {
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        cin >> pos[i] >> alpha[pos[i]];
+    }
+
+    int max_size = 0;
+    for(int i = 0; i < 100; i++) { // 시작점
+        if(alpha[i] != 'H' && alpha[i] !='G') continue;
+        for(int j = i; j<100; j++) { // 끝점
+            if(alpha[j] != 'H' && alpha[j] != 'G') continue;
+            int g = 0, h = 0;
+            for (int k = i; k<=j; k++) {
+                if(alpha[k]=='G') g++;
+                if(alpha[k]=='H')h++;
+            }
+            if(h == 0 && g == 0) continue;
+            if(h+g == 1) continue;
+            if((h== 0 || g ==0) || h==g) {
+                if(j - i >max_size) max_size = j-i;
+            }
+        }
+    }
+    cout << max_size;
+
+    return 0;
+}
